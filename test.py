8f180c44dac4340321aafc1bb37bc0bdd852b32b
VulcanWM <logmedha09@gmail.com>
2022-06-01 08:12:44 +0100

    added a flask app

---
+++ test.py
@@ -0,0 +1,6 @@
+from functions import save_song_data, join_audio, save_chord_data, change_volume
+
+save_chord_data("a:2-c+F:2-", "chords.wav")
+save_song_data("blank-a:1-g:1-F:1", "melody.wav")
+join_audio(["chords.wav", "melody.wav"], "song.wav")
+change_volume("song.wav", "louder.wav", 20)
