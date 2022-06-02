76b9c39774dff434b25e90efe9503f79bd6a7ecc
VulcanWM <logmedha09@gmail.com>
2022-06-02 11:31:30 +0100

    removed flask

--- test.py
+++ test.py
@@ -2,5 +2,7 @@ from functions import save_song_data, join_audio, save_chord_data, change_volume
 
 save_chord_data("a:2-c+F:2-", "chords.wav")
 save_song_data("blank-a:1-g:1-F:1", "melody.wav")
+join_audio(["chords.wav", "melody.wav"], "song2.wav")
+change_volume("melody.wav", "melody.wav", 100)
 join_audio(["chords.wav", "melody.wav"], "song.wav")
-change_volume("song.wav", "louder.wav", 20)
+change_volume("song.wav", "louder.wav", 100)
